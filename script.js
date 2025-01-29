f2b0db75aeca7ffb8549ee9545816243da17ec7a
Pierre-Richard <mayer_alexandre@yahoo.fr>
2025-01-29 23:15:05 +0100

    affichage de la liste dans l'UI

--- script.js
+++ script.js
@@ -22,11 +22,27 @@ let fruits = [
 ];
 fruits.sort();
 console.log("fruits", fruits);
+
+// créer une liste
+let baliseUl = document.createElement("ul");
+
+for (let i = 0; i < fruits.length; i++) {
+  //crée une liste
+  let baliseLi = document.createElement("li");
+  baliseLi.innerHTML = fruits[i];
+  baliseUl.appendChild(baliseLi);
+}
+
+document.querySelector(".container-list").appendChild(baliseUl);
 // 1- recuperer la valeur du input
 let input = document.querySelector("input");
 
 // 2 - ecouter l'evenement sur le champ input
-input.addEventListener("change", () => {
-  console.log("La valeur du champ", input.value);
+input.addEventListener("keyup", (event) => {
+  //
+  let value = event.target.value;
+  if (value === fruits[0]) {
+    return console.log("Valeur", value);
+  }
 });
 console.log("input", input.value);
